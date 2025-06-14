07d2e857abd4322d7371022aa45e94ebdeaa712e
Sam <asamadans@gmail.com>
2025-06-15 02:06:28 +0530

    migrations

---
+++ migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,44 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(MailingList::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(MailingList::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(MailingList::Email)
+                            .string()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(MailingList::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum MailingList {
+    Table,
+    Id,
+    Email,
+}
